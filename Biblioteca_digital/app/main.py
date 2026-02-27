f2688d67a872b117b56638b3cff4782198a1d621
Daniellicea <123047951@upq.edu.mx>
2026-02-27 01:09:40 -0600

    modelos usuarios y validadores

--- Biblioteca_digital/app/main.py
+++ Biblioteca_digital/app/main.py
@@ -7,3 +7,28 @@ app = FastAPI(
     title="API Biblioteca Digital",
     version="1.0.0"
 )
+
+class Usuario(BaseModel):
+    id: int = Field(..., gt=0)
+    nombre: str = Field(..., min_length=3, max_length=50)
+    correo: EmailStr
+
+class Libro(BaseModel):
+    id: int = Field(..., gt=0)
+    nombre: str
+    autor: str
+    año: int
+    paginas: int = Field(..., gt=1)
+    estado: str = "disponible"
+
+    @validator("año")
+    def validar_anio(cls, value):
+        if value < 1450 or value > datetime.now().year:
+            raise ValueError("Año inválido")
+        return value
+
+    @validator("estado")
+    def validar_estado(cls, value):
+        if value not in ["disponible", "prestado"]:
+            raise ValueError("Estado inválido")
+        return value
