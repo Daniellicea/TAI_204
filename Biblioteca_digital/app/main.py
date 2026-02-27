b4c579d94470ccfb97fa49c092f0488266a7f4b8
Daniellicea <123047951@upq.edu.mx>
2026-02-27 01:16:21 -0600

    final

--- Biblioteca_digital/app/main.py
+++ Biblioteca_digital/app/main.py
@@ -8,6 +8,11 @@ app = FastAPI(
     version="1.0.0"
 )
 
+@app.get("/")
+def inicio():
+    return {"mensaje": "API Biblioteca funcionando correctamente"}
+
+
 class Usuario(BaseModel):
     id: int = Field(..., gt=0)
     nombre: str = Field(..., min_length=3, max_length=50)
@@ -42,3 +47,104 @@ class Prestamo(BaseModel):
 usuarios: List[Usuario] = []
 libros: List[Libro] = []
 prestamos: List[Prestamo] = []
+
+
+#USUARI 
+
+@app.post("/v1/usuarios/", status_code=201, tags=["Usuarios"])
+async def agregar_usuario(usuario: Usuario):
+    if any(u.id == usuario.id for u in usuarios):
+        raise HTTPException(status_code=409, detail="El ID del usuario ya existe")
+    usuarios.append(usuario)
+    return {"mensaje": "Usuario registrado correctamente", "usuario": usuario}
+
+
+@app.get("/v1/usuarios/", tags=["Usuarios"])
+async def listar_usuarios():
+    return usuarios
+
+#libros
+
+@app.post("/v1/libros/", status_code=201, tags=["Libros"])
+async def agregar_libro(libro: Libro):
+    if any(l.id == libro.id for l in libros):
+        raise HTTPException(status_code=409, detail="El ID del libro ya existe")
+    libros.append(libro)
+    return {"mensaje": "Libro registrado correctamente", "libro": libro}
+
+
+@app.get("/v1/libros/", tags=["Libros"])
+async def listar_libros():
+    return libros
+
+
+@app.get("/v1/libros/disponibles", tags=["Libros"])
+async def libros_disponibles():
+    disponibles = [l for l in libros if l.estado == "disponible"]
+    return disponibles
+
+
+@app.get("/v1/libros/buscar/{nombre}", tags=["Libros"])
+async def buscar_libro(nombre: str):
+    encontrados = [l for l in libros if nombre.lower() in l.nombre.lower()]
+    if not encontrados:
+        raise HTTPException(status_code=404, detail="Libro no encontrado")
+    return encontrados
+
+#prestamos
+
+@app.post("/v1/prestamos/", status_code=201, tags=["Prestamos"])
+async def registrar_prestamo(prestamo: Prestamo):
+
+    usuario = next((u for u in usuarios if u.id == prestamo.id_usuario), None)
+    if not usuario:
+        raise HTTPException(status_code=404, 
+                            detail="Usuario no existe")
+
+    libro = next((l for l in libros if l.id == prestamo.id_libro), None)
+    if not libro:
+        raise HTTPException(status_code=404, 
+                            detail="Libro no existe")
+
+    if libro.estado == "prestado":
+        raise HTTPException(status_code=409, 
+                            detail="El libro ya está prestado")
+
+    libro.estado = "prestado"
+    prestamos.append(prestamo)
+
+    return {"mensaje": "Préstamo registrado correctamente", 
+            "prestamo": prestamo}
+
+
+@app.put("/v1/prestamos/devolver/{id_libro}", tags=["Prestamos"])
+async def devolver_libro(id_libro: int):
+
+    prestamo = next((p for p in prestamos if p.id_libro == id_libro), None)
+    if not prestamo:
+        raise HTTPException(status_code=409, 
+                            detail="No existe registro de préstamo")
+
+    libro = next((l for l in libros if l.id == id_libro), None)
+    libro.estado = "disponible"
+
+    prestamos.remove(prestamo)
+
+    return {"mensaje": "Libro devuelto correctamente"}  # 200 OK
+
+
+@app.delete("/v1/prestamos/{id_prestamo}", tags=["Prestamos"])
+async def eliminar_prestamo(id_prestamo: int):
+
+    prestamo = next((p for p in prestamos if p.id == id_prestamo), None)
+    if not prestamo:
+        raise HTTPException(status_code=409, 
+                            detail="El préstamo no existe")
+
+    prestamos.remove(prestamo)
+    return {"mensaje": "Préstamo eliminado correctamente"}
+
+
+@app.get("/v1/prestamos/", tags=["Prestamos"])
+async def listar_prestamos():
+    return prestamos
